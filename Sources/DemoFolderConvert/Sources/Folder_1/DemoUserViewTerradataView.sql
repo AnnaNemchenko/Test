8fde8585c65277aa7b66f3a9e8e1ad223220a8f9
AnnaNemchenko <105451361+AnnaNemchenko@users.noreply.github.com>
2022-05-13 13:35:12 +0300

    Delete Sources/DemoFolderConvert/Sources directory

--- Sources/DemoFolderConvert/Sources/Folder_1/DemoUserViewTerradataView.sql
+++
@@ -1,6 +0,0 @@
- CREATE VIEW ViewDemoUsers
-      (FirstName, LastName, PhoneNumber, Email, RegistrationDate) AS
-       SELECT FirstName, LastName, PhoneNumber, Email, RegistrationDate
-       FROM ViewDemoUsers
-       WHERE RegistrationDate <= CURRENT_DATE
-       WITH CHECK OPTION;
